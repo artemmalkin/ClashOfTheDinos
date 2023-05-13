07a530f2218bc506558d57224a1ecbfaa773eae2
grklakg <87272420+grklakg@users.noreply.github.com>
2023-05-13 22:21:49 +0400

    base hp

--- js/game.js
+++ js/game.js
@@ -149,91 +149,86 @@ enemyDinos = [];
 diedDinos = [];
 
 function checkCollisions() {
-/*    if (enemyDinos.length > 0) {*/
-        playerDinos.forEach(dino => {
-            let dinoIndex = playerDinos.indexOf(dino)
+playerDinos.forEach(dino => {
+    let dinoIndex = playerDinos.indexOf(dino)
 
-            if (dinoIndex === 0 && enemyDinos.length > 0) {
-                // If the dinosaur is first
-                const dinoPlayerCollision = playerDinos[0].collision();
-                const dinoEnemyCollision = enemyDinos[0].collision();
-                if (dinoEnemyCollision.x < dinoPlayerCollision.x + dinoPlayerCollision.width) {
-                    // If it reaches the enemy's dinosaur - Fight Mode
-                    if (playerDinos[0].state !== "died") {
-                        if (playerDinos[0].setState("attacking")) {
-                            playerDinos[0].lastAttackTime = Date.now();
-                        }
-                    }
-                    if (enemyDinos[0].state !== "died") {
-                        if (enemyDinos[0].setState("attacking")) {
-                            enemyDinos[0].lastAttackTime = Date.now();
-                        }
-                    }
-                } else {
-                    // If he didn't make it, let him move on.
-                    playerDinos[0].setState("walking");
-                    enemyDinos[0].setState("walking");
-                }
-            } else if (dinoIndex !== 0) {
-                // If not the first one, we check if it has reached the dinosaur walking in front of it
-                const dinoPlayerCollision = playerDinos[dinoIndex].collision();
-                const NextDinoPlayerCollision = playerDinos[dinoIndex - 1].collision();
-                if (NextDinoPlayerCollision.x < dinoPlayerCollision.x + dinoPlayerCollision.width) {
-                    // If reached - stop him
-                    dino.setState("standing");
-                    dino.speed = playerDinos[dinoIndex - 1].speed < dino.speed ? playerDinos[dinoIndex - 1].speed : dino.speed;
-                } else {
-                    // If he didn't make it, let him move on.
-                    dino.setState("walking");
-                    if (NextDinoPlayerCollision.x > dinoPlayerCollision.x + dinoPlayerCollision.width * 1.2) {
-                        dino.speed = dino.originalSpeed;
-                    }
-                }
-            } else {
-                // Check tha last one for the reached the enemy spawnpoint - attacking
-                const dinoPlayerCollision = playerDinos[0].collision();
-                if (dinoPlayerCollision.x + dinoPlayerCollision.width > game.data.spawnpoint.enemy.x - game.cameraWorldPosition.x) {
-                    playerDinos[0].isReachedBase = true;
-                    playerDinos[0].setState("attacking");
+    if (dinoIndex === 0 && enemyDinos.length > 0) {
+        // If the dinosaur is first
+        const dinoPlayerCollision = playerDinos[0].collision();
+        const dinoEnemyCollision = enemyDinos[0].collision();
+        if (dinoEnemyCollision.x < dinoPlayerCollision.x + dinoPlayerCollision.width) {
+            // If it reaches the enemy's dinosaur - Fight Mode
+            if (playerDinos[0].state !== "died") {
+                if (playerDinos[0].setState("attacking")) {
+                    playerDinos[0].lastAttackTime = Date.now();
                 }
             }
-        });
-/*    } else {
-        playerDinos.forEach(dino => dino.setState("walking"))
-    }*/
-
-/*    if (playerDinos.length > 0) {*/
-        enemyDinos.forEach(dino => {
-            let dinoIndex = enemyDinos.indexOf(dino)
-
-            if (dinoIndex !== 0) {
-                // If not the first one, we check if he has reached the dinosaur walking in front of him
-                const dinoEnemyCollision = enemyDinos[dinoIndex].collision();
-                const nextDinoEnemyCollision = enemyDinos[dinoIndex - 1].collision();
-                if (dinoEnemyCollision.x < nextDinoEnemyCollision.x + nextDinoEnemyCollision.width) {
-                    // If he reached - stop him
-                    dino.setState("standing");
-                    dino.speed = enemyDinos[dinoIndex - 1].speed < dino.speed ? enemyDinos[dinoIndex - 1].speed : dino.speed;
-                } else {
-                    // If he didn't make it, let him move on.
-                    dino.setState("walking");
-                    if (nextDinoEnemyCollision.x > dinoEnemyCollision.x + dinoEnemyCollision.width * 1.2) {
-                        dino.speed = dino.originalSpeed;
-                    }
-                }
-            } else {
-                // Check tha last one for the reached the player spawnpoint - attacking
-                const dinoEnemyCollision = enemyDinos[0].collision();
-                if (dinoEnemyCollision.x < game.data.spawnpoint.player.x - game.cameraWorldPosition.x) {
-                    enemyDinos[0].isReachedBase = true;
-                    enemyDinos[0].setState("attacking");
+            if (enemyDinos[0].state !== "died") {
+                if (enemyDinos[0].setState("attacking")) {
+                    enemyDinos[0].lastAttackTime = Date.now();
                 }
             }
-        });
-/*    } else {
-        enemyDinos.forEach(dino => dino.setState("walking"))
-    }*/
-    
+        } else {
+            // If he didn't make it, let him move on.
+            playerDinos[0].setState("walking");
+            enemyDinos[0].setState("walking");
+        }
+    } else if (dinoIndex !== 0) {
+        // If not the first one, we check if it has reached the dinosaur walking in front of it
+        const dinoPlayerCollision = playerDinos[dinoIndex].collision();
+        const NextDinoPlayerCollision = playerDinos[dinoIndex - 1].collision();
+        if (NextDinoPlayerCollision.x < dinoPlayerCollision.x + dinoPlayerCollision.width) {
+            // If reached - stop him
+            dino.setState("standing");
+            dino.speed = playerDinos[dinoIndex - 1].speed < dino.speed ? playerDinos[dinoIndex - 1].speed : dino.speed;
+        } else {
+            // If he didn't make it, let him move on.
+            dino.setState("walking");
+            if (NextDinoPlayerCollision.x > dinoPlayerCollision.x + dinoPlayerCollision.width * 1.2) {
+                dino.speed = dino.originalSpeed;
+            }
+        }
+    } else {
+        // Check tha last one for the reached the enemy spawnpoint - attacking
+        const dinoPlayerCollision = playerDinos[0].collision();
+        if (dinoPlayerCollision.x + dinoPlayerCollision.width > game.data.spawnpoint.enemy.x - game.cameraWorldPosition.x) {
+            playerDinos[0].isReachedBase = true;
+            if (playerDinos[0].setState("attacking")) {
+                playerDinos[0].lastAttackTime = Date.now();
+            }
+        }
+    }
+});
+
+enemyDinos.forEach(dino => {
+    let dinoIndex = enemyDinos.indexOf(dino)
+
+    if (dinoIndex !== 0) {
+        // If not the first one, we check if he has reached the dinosaur walking in front of him
+        const dinoEnemyCollision = enemyDinos[dinoIndex].collision();
+        const nextDinoEnemyCollision = enemyDinos[dinoIndex - 1].collision();
+        if (dinoEnemyCollision.x < nextDinoEnemyCollision.x + nextDinoEnemyCollision.width) {
+            // If he reached - stop him
+            dino.setState("standing");
+            dino.speed = enemyDinos[dinoIndex - 1].speed < dino.speed ? enemyDinos[dinoIndex - 1].speed : dino.speed;
+        } else {
+            // If he didn't make it, let him move on.
+            dino.setState("walking");
+            if (nextDinoEnemyCollision.x > dinoEnemyCollision.x + dinoEnemyCollision.width * 1.2) {
+                dino.speed = dino.originalSpeed;
+            }
+        }
+    } else {
+        // Check tha last one for the reached the player spawnpoint - attacking
+        const dinoEnemyCollision = enemyDinos[0].collision();
+        if (dinoEnemyCollision.x < game.data.spawnpoint.player.x - game.cameraWorldPosition.x) {
+            enemyDinos[0].isReachedBase = true;
+            if (enemyDinos[0].setState("attacking")) {
+                playerDinos[0].lastAttackTime = Date.now();
+            }
+        }
+    }
+}); 
 }
 
 
@@ -251,10 +246,9 @@ function startGame() {
 
     // init data // TODO Base attaking hp
     game.data.spawnpoint = {
-        "player": { "x": 180, "y": 620 },
-        "enemy": { "x": 4100, "y": 620 }
+        "player": { "x": 180, "y": 620, "hp": 1000 },
+        "enemy": { "x": 4200, "y": 620, "hp": 1000 }
     };
-    game.data.dinosMaxCount = 4;
 
     game.data.characterStates = {
         "diplodocus": {
@@ -371,17 +365,25 @@ function fixedUpdate() {
                 playerDinos.splice(playerDinos.indexOf(dino), 1);
             }
             if (dino.state === "attacking") {
-                if (enemyDinos.length > 0) {
-                    if (now - dino.lastAttackTime > dino.attackDuration) {
+
+                if (now - dino.lastAttackTime > dino.attackDuration) {
+                    if (enemyDinos.length > 0) {
                         enemyDinos[0].hp -= dino.attackDamage;
-                        dino.lastAttackTime = now;
+                        
                         console.log("Player is attacking, Enemy hp: " + enemyDinos[0].hp);
                         if (enemyDinos[0].hp <= 0) {
                             dino.setState("walking");
                         }
+                    } else if (dino.isReachedBase) {
+                        game.data.spawnpoint.enemy.hp -= dino.attackDamage;
+                        console.log("Player is attacking the base, Base hp: " + game.data.spawnpoint.enemy.hp);
+                        if (game.data.spawnpoint.enemy.hp <= 0) {
+                            alert("You win!")
+                        }
+                    } else {
+                        dino.setState("walking");
                     }
-                } else if (!dino.isReachedBase) {
-                    dino.setState("walking");
+                    dino.lastAttackTime = now;
                 }
             }
             dino.x += dino.state === "walking" ? dino.speed : 0;
@@ -394,18 +396,26 @@ function fixedUpdate() {
                 enemyDinos.splice(enemyDinos.indexOf(dino), 1);
             }
             if (dino.state === "attacking") {
-                if (playerDinos.length > 0) {
-                    if (now - dino.lastAttackTime > dino.attackDuration) {
+                
+                if (now - dino.lastAttackTime > dino.attackDuration) {
+                    if (playerDinos.length > 0) {
                         playerDinos[0].hp -= dino.attackDamage;
-                        dino.lastAttackTime = now;
                         console.log("Enemy is attacking, Player hp: " + playerDinos[0].hp);
                         if (playerDinos[0].hp <= 0) {
                             dino.setState("walking");
                         }
+                    } else if (dino.isReachedBase) {
+                        game.data.spawnpoint.player.hp -= dino.attackDamage;
+                        console.log("Enemy is attacking the base, Base hp: " + game.data.spawnpoint.player.hp);
+                        if (game.data.spawnpoint.player.hp <= 0) {
+                            alert("You loose!")
+                        }
+                    } else {
+                        dino.setState("walking");
                     }
-                } else if (!dino.isReachedBase) {
-                    dino.setState("walking");
+                    dino.lastAttackTime = now;
                 }
+                
             }
             dino.x += dino.state === "walking" ? -dino.speed : 0;
         });
